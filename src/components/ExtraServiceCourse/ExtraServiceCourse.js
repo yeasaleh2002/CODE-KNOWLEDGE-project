6226a9e032be96896499998919905e0c927b3bd5
Yea Saleh <yeasalehofficial2002@gmail.com>
2021-10-04 03:13:44 +0600

    json file  commit

---
+++ src/components/ExtraServiceCourse/ExtraServiceCourse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExtraServiceCourse = () => {
+    return (
+        <div>
+            <h1>extra</h1>
+        </div>
+    );
+};
+
+export default ExtraServiceCourse;
